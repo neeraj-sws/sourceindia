2b9b4ae1e241fe9a0d2fdcc27be75a3cce6f90e3
neeraj-sws <131853089+neeraj-sws@users.noreply.github.com>
2025-10-15 20:00:57 +0530

    front end pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,42 +1,9 @@
-import axios from "axios";
-import API_BASE_URL, { ROOT_URL } from "./../config";
-import ImageWithFallback from "../admin/common/ImageWithFallback";
-import CompaniesCarousel from './common/CompaniesCarousel';
 import Banner from '../sections/Banner';
 import Category from '../sections/Category';
 import Product from '../sections/Product';
 import Company from '../sections/Company';
-import React, { useEffect, useState } from 'react'
 
 const Home = () => {
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
-
-
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await axios.get(`${API_BASE_URL}/products?limit=6`);
-        setProducts(res.data.products);
-      } catch (err) {
-        console.error("Error fetching categories:", err);
-      }
-    };
-    fetchProducts();
-  }, []);
-
-  useEffect(() => {
-    const fetchCompanies = async () => {
-      try {
-        const res = await axios.get(`${API_BASE_URL}/products/companies?limit=16`);
-        setCompanies(res.data);
-      } catch (err) {
-        console.error("Error fetching companies:", err);
-      }
-    };
-    fetchCompanies();
-  }, []);
 
   return (
     <>
@@ -45,7 +12,6 @@ const Home = () => {
       <Category />
       <Product />
       <Company />
-
     </>
   )
 }
