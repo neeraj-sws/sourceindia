be078c2d43555bcc7e741c60e9d223479097c715
lalit-sws <lbaingnesws@gmail.com>
2025-10-27 16:44:02 +0530

    update profile & company

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -58,7 +58,7 @@ const Users = sequelize.define('Users', {
   is_company: { type: DataTypes.TINYINT, allowNull: true, defaultValue: 1, },
   is_product: { type: DataTypes.TINYINT, allowNull: true, defaultValue: 1, },
 }, {
-  tableName: 'users1',
+  tableName: 'users',
   timestamps: true,
   createdAt: 'created_at',
   updatedAt: 'updated_at'
